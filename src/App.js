57fe25a9e6d4de48ea58ec6a31dba2dbab711a8b
itsaimal <itsaimalyousafzai@gmail.com>
2020-12-14 00:00:31 +0500

    nav construction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-  <h1>hello</h1>
+  <Nav/>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleshow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleshow(true)
+      } else handleshow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll")
+    }
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://angular.io/assets/images/logos/angular/angular.png"
+        alt="AK Logo"
+      />
+      <img
+        className="nav__avatar"
+       
+      />
+    </div>
+  )
+}
+
+export default Nav
